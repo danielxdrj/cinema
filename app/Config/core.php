ee870dcb630822582d277ef47ffbada5133fd7d7
Carlos <danielxdrj20@gmail.com>
2025-12-31 01:30:33 -0300

    chore(security): atualiza salt e cipherSeed

--- app/Config/core.php
+++ app/Config/core.php
@@ -233,12 +233,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'DYhGfG7g8dneSm0j7q3v39VwW4yX6pHwsqb3Evb9Lzg8a9ndu0fa');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '76859309657453542496723424863');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
