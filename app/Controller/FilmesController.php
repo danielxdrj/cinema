d3cfac12cc4df92cfaf48b77f31c9fdf29b3d80f
Carlos <danielxdrj20@gmail.com>
2026-01-25 19:13:39 -0300

    feat(add): adicionar filmes

--- app/Controller/FilmesController.php
+++ app/Controller/FilmesController.php
@@ -6,17 +6,26 @@ class FilmesController extends AppController {
 
 
     public function index() {
-    $filmes = array(
-        array('Filme' => array('nome' => 'Avengers', 'ano' => '2019', 'duracao' => '2:58:37', 'idioma' => 'Inglês')),
-        array('Filme' => array('nome' => 'Spider-Man', 'ano' => '2019', 'duracao' => '2:04:00', 'idioma' => 'Português')),
-        array('Filme' => array('nome' => 'Batman v Superman', 'ano' => '2016', 'duracao' => '2:32:00', 'idioma' => 'Inglês')),
-        array('Filme' => array('nome' => 'Iron Man 3', 'ano' => '2013', 'duracao' => '2:15:00', 'idioma' => 'Inglês')),
-        array('Filme' => array('nome' => 'The Dark Knight', 'ano' => '2008', 'duracao' => '2:32:00', 'idioma' => 'Português')),
-        array('Filme' => array('nome' => 'Captain America: The First Avenger', 'ano' => '2011', 'duracao' => '2:04:00', 'idioma' => 'Inglês')),
-        array('Filme' => array('nome' => 'Thor', 'ano' => '2011', 'duracao' => '1:54:00', 'idioma' => 'Português'))
-    );
+
+    $fields = ['Filme.nome', 'Filme.ano','Filme.duracao','Filme.idioma'];    
+    $order = ['Filme.ano' => 'desc'];     
+    $filmes = $this->Filme->find('all', compact('fields','order'));
     
     $this->set('filmes', $filmes);
     }
+
+
+      public function add()
+     {
+        if(!empty($this->request->data)){
+            $this->Filme->create();
+           if($this->Filme->save($this->request->data)){
+              $this->Flash->set("Filme gravado com sucesso");
+              $this->Redirect('/Filmes');
+              }
+        }
+     }
+
+   
 
 }
