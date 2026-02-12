d61f23bf32c3ee3ecd56865d16ed18f674035fd5
Carlos <danielxdrj20@gmail.com>
2026-02-12 03:56:58 -0300

    fix: belongsTo ausente

--- app/Model/Filme.php
+++ app/Model/Filme.php
@@ -2,5 +2,10 @@
 App::uses('AppModel', 'Model');
 
 class Filme extends AppModel{
-
+  public $belongsTo = [
+        'Genero' => [
+            'className' => 'Genero',
+            'foreignKey' => 'genero_id'
+        ]
+    ];
 }
